35808f8fe765eb912c5253fc531ac942605b03d3
Diego Mena <ingelectronicadj@gmail.com>
2021-03-23 21:02:26 -0500

    inicio desarrollo de landing page

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    // Menu Responsive
+    $(".menu-responsive").toggle();
+
+    $(".menu-bars").on("click", function() {
+        $(".menu-responsive").toggle("slow");
+    });
+
+
+    // Scroll click on menu
+    $(".scroll").on("click", function() {
+        $(this).each(function() {
+            const sectionTop = $(this.hash).offset().top;
+            $("html, body").animate({
+                scrollTop: sectionTop
+            }, 1500);
+        });
+    });
+
+});
