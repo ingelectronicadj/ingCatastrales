9da214a7d22c6f734be46bfbe834880837805a67
Diego Mena <ingelectronicadj@gmail.com>
2021-03-28 16:36:53 -0500

    slider para seccion de servicios implementado con Slick

--- js/main.js
+++ js/main.js
@@ -41,3 +41,37 @@ btn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '300');
 });
+
+
+/**Slick Corousel**/
+$(document).ready(function() {
+    $('.carousel').slick({
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        centerMode: true,
+        responsive: [{
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: true,
+                infinite: true,
+
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        }]
+    });
+});
