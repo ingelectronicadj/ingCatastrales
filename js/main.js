63976c254a3c0f6624cbec34746dbeebcb54008f
Diego Mena <ingelectronicadj@gmail.com>
2021-04-24 17:33:32 -0500

    ajuste al texto de servicio de drones

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
         speed: 500,
         slidesToShow: 2,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000,
         dots: false,
         centerMode: true,
