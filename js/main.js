b52c9a36d9f978d9feadee0a01522b039e441f0c
Diego Mena <ingelectronicadj@gmail.com>
2021-05-04 22:03:25 -0500

    arreglo vista movil

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
         speed: 500,
         slidesToShow: 2,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000,
         dots: false,
         centerMode: true,
@@ -95,7 +95,7 @@ $(document).ready(function() {
                 slidesToScroll: 1,
                 dots: false,
                 infinite: true,
-                autoplay: false,
+                autoplay: true,
                 autoplaySpeed: 3000,
             }
         }]
